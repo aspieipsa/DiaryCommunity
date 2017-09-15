5cb067ebd4fcbf6be24e735f0beb24c1c879ce98
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2017-09-15 18:25:53 +0300

    Make the course example work on this setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Courseware/style.css";
+import App from "./Courseware/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
+  module.hot.accept("./Courseware/App", () => {
+    ReactDOM.render(<App />, document.getElementById("root"));
   });
 }
