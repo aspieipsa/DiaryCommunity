11c17a98653843179ed0a564df38877a53073709
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2017-11-01 00:26:54 +0200

    Comment out unfinished test work

--- models/__tests__/user.test.js
+++ models/__tests__/user.test.js
@@ -1,24 +1,21 @@
-import startServer from '../../server';
+/*import startServer from '../../server'; // will not work now, need to wrap the server and export the function (not that easy as sounds)
 import mongoose from 'mongoose';
 mongoose.Promise = Promise;
 //import { getUserConstructor, generateUser } from './helpers/utils';
 import User from '../User';
 
 let server;
+
+// TODO: connect to a test database
 beforeAll(() => startServer().then(s => (server = s)));
+// TODO: delete collections and disconnect
 afterAll(done => server.close(done));
 
 describe('Creating records', () => {
-  it('saves a user', async () => {
+  xit('saves a user', async () => {
     const u = new User({ email: 'aa@aa.fi', password: 'aaaaaa' });
-    console.log('hello');
     await u.save();
     expect(u.isNew).toBeFalsy();
-
-    /*return github.getUser('vnglst')
-    .then(data => {
-      expect(data).toBeDefined()
-      expect(data.entity.name).toEqual('Koen van Gilst')
-    })*/
   });
 });
+*/
